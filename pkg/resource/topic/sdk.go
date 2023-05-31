8aa51980c0b3476e11ceb38b3345dce0209be538
mustafasencer <m.sencerozcan@gmail.com>
2023-05-31 14:46:45 +0200

    Build controller

--- pkg/resource/topic/sdk.go
+++ pkg/resource/topic/sdk.go
@@ -105,7 +105,7 @@ func (rm *resourceManager) sdkFind(
 
 	// If the Name field is empty, populate it with the last part of the topic ARN
 	// This is a workaround for the fact that the Name field is required by the
-	// Name API call, but not by the GetTopicAttributes API call
+	// CreateTopic API call, but not by the GetTopicAttributes API call
 	// Use case: adopting an existing topic by topic ARN
 	if ko.Spec.Name == nil {
 		topicName, err := rm.getTopicNameFromARN(tmpARN)
